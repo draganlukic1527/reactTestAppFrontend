8ce2aad20530220cfff432f1f8aec631d5ade7bc
draganlukic1527 <draganlukic1527@gmail.com>
2023-09-25 23:05:50 -0700

    added updated ui and github actions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState([]);
+
+	async function handleLogin(credentials) {
+		return await fetch("http://localhost:8080/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(credentials),
+		})
+			.then((data) => {
+				return data.json();
+			})
+			.catch((error) => console.log("ERROR:", error));
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setMessage(await handleLogin({ username, password }));
+	};
+
+	return (
+		<div className="App">
+			<div></div>
+			<h1>Login App</h1>
+			<div>
+				<input
+					type="text"
+					placeholder="Username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+			</div>
+			<div>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+			</div>
+			<button onClick={handleSubmit}>Login</button>
+			<h2>Welcome {message.message}.</h2>
+		</div>
+	);
 }
 
 export default App;
